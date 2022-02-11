3af5a8dd64ab94b130cabdc7124e8e81645d4ad7
Chubby <magmarelli@web.de>
2022-02-11 14:41:56 +0100

    fix: fix description

---
+++ .github/workflows/release.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  branches: "main",
+  repositoryURL: "https://github.com/Chubaker/my-app",
+  plugins: [
+    '@semantic-release/commit-analyzer', 
+    '@semantic-release/release-notes-generator', 
+    '@semantic-release/github']
+}
